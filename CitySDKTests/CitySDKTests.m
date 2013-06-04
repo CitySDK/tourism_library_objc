d18d84753cfbec785398db1bd8fcc76f6a0eaee3
pcruz7 <pedro.cruz.sousa@gmail.com>
2013-06-04 22:49:00 +0100

    added copyright info

--- CitySDKTests/CitySDKTests.m
+++ CitySDKTests/CitySDKTests.m
@@ -1,11 +1,23 @@
-//
-//  CitySDKTests.m
-//  CitySDKTests
-//
-//  Created by Pedro Cruz Sousa on 01/04/13.
-//  Copyright (c) 2013 Pedro Cruz Sousa. All rights reserved.
-//
-
+/*
+ * COPYRIGHT NOTICE:
+ *
+ * This file is part of CitySDK WP5 Tourism Objective-C Library.
+ *
+ * CitySDK WP5 Tourism Objective-C Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CitySDK WP5 Tourism Objective-C Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CitySDK WP5 Tourism Objective-C Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2013, IST
+ */
 #import "CitySDKTests.h"
 
 NSString* const url = @"http://polar-lowlands-9873.herokuapp.com/?list=backend";
